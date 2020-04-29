4d16b63b7a6696c9d6f329668ed0e844d989e897
Tim Knapp <timknapp12@gmail.com>
2020-04-28 23:42:54 -0600

    add mock data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { styles, colors, seats, wheels } from "./mocks";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 4rem;
+  flex-direction: column;
+`;
+
+const CheckboxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
+  const [style, setStyle] = useState("");
+  console.log("style", style);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <div>
+        <label>Choose a style</label>
+        <input
+          name="style"
+          list="styles"
+          onChange={(e) => setStyle(e.target.value)}
+        />
+        <datalist id="styles">
+          {styles.map((item) => (
+            <option key={item.id} value={item.name} />
+          ))}
+        </datalist>
+        Searching for: {style}
+      </div>
+      <div></div>
+      <div>
+        Colors
+        <CheckboxWrapper>
+          {colors.map((color) => (
+            <label key={color.id}>
+              <input type="checkbox" name={color.name} />
+              {color.name}
+            </label>
+          ))}
+        </CheckboxWrapper>
+      </div>
+      <div>
+        Seats
+        <CheckboxWrapper>
+          {seats.map((seat) => (
+            <label key={seat.id}>
+              <input type="checkbox" name={seat.name} />
+              {seat.name}
+            </label>
+          ))}
+        </CheckboxWrapper>
+      </div>
+      <div>
+        Wheels
+        <CheckboxWrapper>
+          {wheels.map((wheel) => (
+            <label key={wheel.id}>
+              <input type="checkbox" name={wheel.name} />
+              {wheel.name}
+            </label>
+          ))}
+        </CheckboxWrapper>
+      </div>
+    </Wrapper>
   );
 }
 
---
+++ src/mocks.js
@@ -0,0 +1,36 @@
+export const styles = [
+  { id: "1", name: "Style-1" },
+  { id: "2", name: "Style-2" },
+  { id: "3", name: "Style-3" },
+  { id: "4", name: "Style-4" },
+  { id: "5", name: "Style-5" },
+  { id: "6", name: "Style-6" },
+  { id: "7", name: "Style-7" },
+  { id: "8", name: "Style-8" },
+  { id: "9", name: "Style-9" },
+  { id: "10", name: "Style-10" },
+];
+
+export const colors = [
+  { id: 101, name: "Glacier Silver" },
+  { id: 102, name: "Estoril Blue II" },
+  { id: 103, name: "Havana / Mojave Metallic" },
+  { id: 104, name: "Alpine White" },
+  { id: 105, name: "Imperial Blue" },
+  { id: 106, name: "Liquid Blue" },
+  { id: 107, name: "Melbourne Red" },
+];
+
+export const seats = [
+  { id: 201, name: "standard" },
+  { id: 202, name: "heated" },
+  { id: 203, name: "racing" },
+  { id: 204, name: "bucket" },
+];
+
+export const wheels = [
+  { id: 301, name: "M1 TRX Campagnolo" },
+  { id: 302, name: "TRX 1" },
+  { id: 303, name: "TRX 2" },
+  { id: 304, name: "E30" },
+];
